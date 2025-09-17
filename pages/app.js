4ce581edf9b81872a8ead9c1e8c957afa8d084ec
Jirapinyaa <s6506021622191@email.kmutnb.ac.th>
2025-09-17 18:33:52 +0700

    Third

--- pages/app.js
+++ pages/app.js
@@ -1,21 +1,22 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js")
-    .then(() => console.log("✅ Service Worker registered"));
+    navigator.serviceWorker.register("/service-worker.js")
+        .then(() => console.log("✅ Service Worker registered"))
+        .catch(err => console.error("Service Worker failed:", err));
 }
 
-document.getElementById("loadBtn").addEventListener("click", async () => {
-  const gallery = document.getElementById("gallery");
-  gallery.innerHTML = "⏳ กำลังโหลด...";
+document.getElementById("loadBtn").addEventListener("click", async() => {
+    const gallery = document.getElementById("gallery");
+    gallery.innerHTML = "⏳ กำลังโหลด...";
 
-  const res = await fetch("/api/images");
-  const cloudImages = await res.json();
+    const res = await fetch("/api/images");
+    const cloudImages = await res.json();
 
-  gallery.innerHTML = "";
-  cloudImages.forEach(url => {
-    const img = document.createElement("img");
-    img.src = url;
-    img.style.width = "300px";
-    img.style.margin = "5px";
-    gallery.appendChild(img);
-  });
+    gallery.innerHTML = "";
+    cloudImages.forEach(url => {
+        const img = document.createElement("img");
+        img.src = url;
+        img.style.width = "300px";
+        img.style.margin = "5px";
+        gallery.appendChild(img);
+    });
 });
